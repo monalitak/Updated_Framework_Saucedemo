d3a3f35d0f7e2746f4673d12fecf3e343855d3f3
monalitak <monalitak@gmail.com>
2025-08-28 14:27:37 +0100

    Update test_lockedout_user.py

--- Test/test_lockedout_user.py
+++ Test/test_lockedout_user.py
@@ -9,7 +9,8 @@ from SauceDemo_Framework.pageObjects.login import LoginPage
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import time
 
-test_data_path = '../Data/test_locked_user.json'
+base_dir = os.path.dirname(os.path.abspath(__file__))
+test_data_path = os.path.join(base_dir, '..', 'SauceDemo_Framework', 'Data', 'test_data.json')
 with open(test_data_path) as f:
     test_data = json.load(f)
     test_list = test_data["data"]
@@ -19,3 +20,4 @@ def test_locked_user (browserInstance, base_url, test_list_item):
     driver = browserInstance
     loginPage = LoginPage(driver, base_url)
     loginPage.locked_user(test_list_item["userid"], test_list_item["password"])
+
